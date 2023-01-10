d8b31927ac94c32f4f9726ca2cd75ca29e984d5b
Dongjin Kim <nyong9221@gmail.com>
2023-01-11 01:27:22 +0900

    Init: SpringBoot init

---
+++ BE/Animalcenter/src/main/java/tworeal/Animalcenter/AnimalcenterApplication.java
@@ -0,0 +1,13 @@
+package tworeal.Animalcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalcenterApplication.class, args);
+	}
+
+}
---
+++ BE/Animalcenter/src/test/java/tworeal/Animalcenter/AnimalcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package tworeal.Animalcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
